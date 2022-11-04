73c1d25f16909cb0006b2b13accaf1c7b0bd6308
Hukenis <95994303+Hukenis@users.noreply.github.com>
2022-11-04 16:40:00 +0800

    ping 工具
    可以ping 一个到多个ip，支持批量 ping 一到多个IP清单文件 (必须纯 ip文本，允许有空格或换行符存在。)

---
+++ Shell_Scripts/1.system_tools/ping_tool.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Date  : 2022_11_04
+# Auther: wukai@sandstar.com
+
+
+create_ip_logDir(){
+ip_logDir="/home/sandstar/wukai/ping_log"
+mkdir -p ${ip_logDir}
+
+Today_dir="${ip_logDir}/`date  +%Y%m%d`"
+mkdir -p ${Today_dir}
+
+# date  +[%Y%m%d_%H:%M:%S][info]
+}
+
+create_ip_logDir
+
+fuction_ping(){
+
+if [[  -z $1  ]];then 
+
+	read -p "Please specify the test file or ip_address :   " machine_ip
+else
+	machine_ip=$1
+fi
+
+if [[  -f ${machine_ip}  ]];then 
+	# echo ${machine_ip} is a file
+	ip=`cat ${machine_ip}` 
+	echo -e "`date  +[%Y%m%d_%H:%M:%S][info]` : Ping_command Debug log  " >>${Today_dir}/ping_all.log
+
+	for i in ${ip};
+	do	
+		ping -c 3  ${i} >>  ${Today_dir}/ping_all.log && echo -e "\n ################# "
+			if	[   $? -eq 0 ];then 
+				echo -e "`date  +[%Y%m%d_%H:%M:%S][info]`  $i is ok "|tee -a  ${Today_dir}/ping_tong.log
+			else
+				echo -e "\n ################# "
+				echo -e "`date  +[%Y%m%d_%H:%M:%S][warring]` $i is Unreachable " | tee -a  ${Today_dir}/ping_failed.log
+			fi
+	done
+else
+	# echo ${machine_ip} is not file 
+	for i in  ${machine_ip}
+	do
+		ping -c 3  ${i} >>  ${Today_dir}/ping_all.log && echo -e "\n ################ "
+		if      [   $? -eq 0 ];then
+                                echo  -e "`date  +[%Y%m%d_%H:%M:%S][info]` ${i} is ok " |tee -a  ${Today_dir}/ping_tong.log
+                        else
+				                        echo -e "\n ################# "
+                                echo  -e "`date  +[%Y%m%d_%H:%M:%S][warring]` ${i} is Unreachable " | tee -a  ${Today_dir}/ping_failed.log
+                fi
+	done 
+
+fi
+}
+
+function_ping
